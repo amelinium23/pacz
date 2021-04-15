9f8a65e3e094e6577d113adbe435a092af3d3ec0
Piotr Tomczak <230025@edu.p.lodz.pl>
2021-04-15 09:02:40 +0200

    Main screen changed

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,18 +5,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.util.Objects;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
-        primaryStage.setTitle("Aplikacja do zarządzania kinem");
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainScreen.fxml")));
+        primaryStage.setTitle("Podstawy analizy cyklu życia");
         primaryStage.setResizable(false);
-        primaryStage.setScene(new Scene(root, 600, 250));
+        primaryStage.setScene(new Scene(root, 500, 150));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
--- src/sample/MainScreenController.java
+++ src/sample/MainScreenController.java
@@ -1,4 +1,13 @@
 package sample;
 
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class MainScreenController {
+    public Button quitButton;
+
+    public void quit() {
+        Stage stage = (Stage) quitButton.getScene().getWindow();
+        stage.close();
+    }
 }
