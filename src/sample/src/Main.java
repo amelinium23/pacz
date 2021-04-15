883af8d313550df3df1e5d59ddf059b16180b262
Piotr Tomczak <230025@edu.p.lodz.pl>
2021-04-15 08:30:45 +0200

    Fixing project!

--- src/sample/src/Main.java
+++ src/sample/src/Main.java
@@ -5,12 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("./ScreeningRoomView.fxml")));
+        primaryStage.setTitle("Chuj");
         primaryStage.setResizable(false);
         primaryStage.show();
     }
